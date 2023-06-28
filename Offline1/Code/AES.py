dbc39abe67eac4b6b8baf1b7bccf737410874560
Fabiha Tasneem <1805072@ugrad.cse.buet.ac.bd>
2023-06-28 16:20:16 +0600

    AES Encryption done

--- Offline1/Code/AES.py
+++ Offline1/Code/AES.py
@@ -1,29 +1,15 @@
-from BitVector import *
-from bitvectordemo import *
-from keySchedulingAlgo import *
-from util import *
+from encryption import *
+from decryption import *
 
 #take input string
 input_string = input("Enter a plain text: ")
+# chunk the string into 16 byte chunks
+plain_text_chunks = chunk_string(input_string)
+
+#take input key
 input_key = input("Enter a key: ")
 hex_key = get_hex_ascii(input_key)
 # key = key_schedule(key)
 
-# each 4 elements in the hex_key list is a word
-word_list = []
-for i in range(0, len(hex_key)-3, 4):
-    word_list.append(hex_key[i:i + 4])
+print("Desired Ciphertext: ", encryption(plain_text_chunks, hex_key))
 
-# XOR with the previous round key and store the result
-for i in range(10):
-    print("Round ", i+1)
-    curr = []
-    g_word_3 = modify_last_word(word_list[3])
-    print("g_word_3 : ",g_word_3)
-    curr.append(XOR(g_word_3, word_list[0]))
-    curr.append(XOR(curr[0], word_list[1]))
-    curr.append(XOR(curr[1], word_list[2]))
-    curr.append(XOR(curr[2], word_list[3]))
-    word_list = curr
-    print(word_list)
-    print("----------------------------------------------------------------------------------")
--- Offline1/Code/util.py
+++ Offline1/Code/util.py
@@ -28,33 +28,92 @@ def byte_substitution(word):
     new_word = []
     # Perform byte substitution using the S-box
     for i in range(len(word)):
-        print("word[i]: ",word[i])
-        row = int(str(word[i][0]), 16)  # Convert the first character of byte to decimal
-        if(word[i][1] == None):
-            col = 0
+        if(len(word[i]) < 2):
+            row = 0
+            col = int(str(word[i][0]), 16)
         else:
+            row = int(str(word[i][0]), 16)  # Convert the first character of byte to decimal
             col = int(str(word[i][1]), 16)  # Convert the second character of byte to decimal
         new_word.append(hex(Sbox[row*16 + col])[2:])
     return new_word
 
-def add_round_constant(word):
-    constant = [1, 00, 00, 00]
-    return XOR(word, constant)
+# write a function for byte substitution for all entries of a matrix from the Sbox in bitvectordemo.py
+def byte_substitution_matrix(matrix):
+    new_matrix = []
+    for i in range(4):
+        new_matrix.append(byte_substitution(matrix[i]))
+    return new_matrix
 
-def XOR(word1, word2):
-    print("word1: ",word1," , word2: ", word2)
+def add_round_constant(round, word):
+    constant = Rcon[round]
     result = []
-    for i in range(len(word2)):
-        num1 = int(str(word1[i]), 16)
-        num2 = int(str(word2[i]), 16)
-        result.append(hex(num1 ^ num2)[2:])
+    for i in range(len(word)):
+        num = int(str(word[i]), 16)
+        result.append(hex(num ^ constant[i]))
     return result
 
-def modify_last_word(word):
+def XOR_hexa(num1, num2):
+    num1 = int(num1, 16)
+    num2 = int(num2, 16)
+    return hex(num1 ^ num2)[2:]
+
+def XOR_word(word1, word2):
+    result = []
+    for i in range(len(word2)):
+        result.append(XOR_hexa(str(word1[i]), str(word2[i])))
+    return result
+
+def modify_last_word(round, word):
     word = left_shift(word)
-    print(word)
     word = byte_substitution(word)
-    print(word)
-    word = add_round_constant(word)
-    print(word)
+    word = add_round_constant(round, word)
     return word
+
+def turn_into_column_matrix(word_list):
+    matrix = []
+    for i in range(4):
+        column = []
+        for j in range(4):
+            column.append(word_list[j][i])
+        matrix.append(column)
+    return matrix
+
+def XOR_matrix(matrix1, matrix2):
+    result = [ [0]*4 for i in range(4)]
+    for i in range(4):
+        for j in range(4):
+            result[i][j] = XOR_hexa(matrix1[i][j], matrix2[i][j])
+    return result
+
+#four rows are shifted cyclically to the left by offsets of 0,1,2, and 3
+def shift_rows_cyclically(matrix):
+    result = [ [0]*4 for i in range(4)]
+    for i in range(4):
+        for j in range(4):
+            result[i][j] = matrix[i][(j+i)%4]
+    return result
+
+#multiply two hexadecimal numbers
+def multiply_hexa(num1, num2):
+    num1 = int(str(num1), 16)
+    num2 = int(str(num2), 16)
+    return hex(num1 * num2)[2:]
+
+#Mix Column multiplies fixed matrix Mixer from bitvectordemo.py against current State Matrix
+def mix_columns(matrix):
+    result = [ [0]*4 for i in range(4)]
+    for i in range(4):
+        for j in range(4):
+            result[i][j] = XOR_hexa((BitVector(bitstring=Mixer[i][0]).gf_multiply_modular(BitVector(hexstring=matrix[0][j]), AES_modulus, 8)).get_bitvector_in_hex(),
+                XOR_hexa((BitVector(bitstring=Mixer[i][1]).gf_multiply_modular(BitVector(hexstring=matrix[1][j]),  AES_modulus, 8)).get_bitvector_in_hex(), 
+                XOR_hexa((BitVector(bitstring=Mixer[i][2]).gf_multiply_modular(BitVector(hexstring=matrix[2][j]),  AES_modulus, 8)).get_bitvector_in_hex(),
+                (BitVector(bitstring=Mixer[i][3]).gf_multiply_modular(BitVector(hexstring=matrix[3][j]),  AES_modulus, 8)).get_bitvector_in_hex())))
+    return result
+
+# convert the column matrix into a list of words
+def convert_matrix_to_words(matrix):
+    word_list = []
+    for i in range(4):
+        for j in range(4):
+            word_list.append(matrix[j][i])
+    return word_list
